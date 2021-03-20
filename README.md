f07c12e0e5cdf78e1f2b9d7d6095338206c846ee
Nicolò Giuliani <info@ngiuliani.com>
2021-03-20 14:23:24 +0100

    Standard structure.

--- README.md
+++ README.md
@@ -7,3 +7,8 @@ A new Flutter application. Fudeo exercises.
 - Lessons:
     - Lesson 3.1: https://bit.ly/3s8TKru
         - Argument: **Introduzione Sliver**
+    - Lesson 3.2: https://bit.ly/38UXSnB
+        - Argument: **Home Page**
+    - Lesson 3.3: https://bit.ly/3vJB7g9
+        - Argument: **Profile Page**
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(App());
@@ -8,7 +9,10 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: true,
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        brightness: Brightness.dark,
+      ),
       home: HomePage(),
     );
   }
@@ -17,6 +21,54 @@ class App extends StatelessWidget {
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: appBar(),
+      body: contactList(context),
+    );
+  }
+
+  Widget appBar() {
+    return AppBar(
+      title: Text("Sliver"),
+      centerTitle: true,
+      leading: IconButton(
+        icon: Icon(
+          Icons.dehaze,
+        ),
+        onPressed: () {},
+      ),
+      actions: [
+        IconButton(
+          icon: Icon(
+            Icons.search,
+          ),
+          onPressed: () {},
+        ),
+      ],
+    );
+  }
+
+  Widget contactList(BuildContext context) {
+    return ListView.builder(
+      itemCount: 50,
+      itemBuilder: (context, index) => contactCard(index),
+    );
+  }
+
+  Widget contactCard(int index) {
+    return ListTile(
+      leading: CircleAvatar(
+        radius: 25,
+        backgroundColor: Colors.grey.shade600,
+      ),
+      title: Text("Contact $index"),
+      subtitle: Text("Hello!"),
+      trailing: Text(
+        "Now",
+        style: TextStyle(
+          fontSize: 12,
+        ),
+      ),
+    );
   }
 }
