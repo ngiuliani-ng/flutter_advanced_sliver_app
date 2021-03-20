e1f1ab8725a8c66fa153ad66f00ddec6ede942c3
Nicolò Giuliani <info@ngiuliani.com>
2021-03-20 15:17:18 +0100

    Project organization.

--- README.md
+++ README.md
@@ -9,6 +9,7 @@ A new Flutter application. Fudeo exercises.
         - Argument: **Introduzione Sliver**
     - Lesson 3.2: https://bit.ly/38UXSnB
         - Argument: **Home Page**
+            - Screenshot App: Screenshot\Screenshot_1616249027.png
     - Lesson 3.3: https://bit.ly/3vJB7g9
         - Argument: **Profile Page**
-
+            - Screenshot App: 
---
+++ lib/pages/home.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          appBar(),
+          contactList(context),
+        ],
+      ),
+    );
+  }
+
+  Widget appBar() {
+    return SliverAppBar(
+      title: Text("Sliver"),
+      centerTitle: true,
+
+      /// Se [floating] è "true" ci permette di far comparire
+      /// la [SliverAppBar] senza tornare completamente in alto.
+      floating: true,
+
+      /// Se [snap] è "true" ci permette di far comparire
+      /// la [SliverAppBar] appena scrolliamo.
+      snap: true,
+      leading: IconButton(
+        icon: Icon(
+          Icons.dehaze,
+        ),
+        onPressed: () {},
+      ),
+      actions: [
+        IconButton(
+          icon: Icon(
+            Icons.search,
+          ),
+          onPressed: () {},
+        ),
+      ],
+    );
+  }
+
+  Widget contactList(BuildContext context) {
+    return SliverList(
+      delegate: SliverChildBuilderDelegate(
+        (context, index) => contactCard(index),
+        childCount: 50,
+      ),
+    );
+  }
+
+  Widget contactCard(int index) {
+    return ListTile(
+      leading: CircleAvatar(
+        radius: 25,
+        backgroundColor: Colors.grey.shade600,
+      ),
+      title: Text("Contact $index"),
+      subtitle: Text("Hello!"),
+      trailing: Text(
+        "Now",
+        style: TextStyle(
+          fontSize: 12,
+        ),
+      ),
+    );
+  }
+}
